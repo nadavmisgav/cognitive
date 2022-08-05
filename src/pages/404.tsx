abc0395d32b3f93aa1c0ca80b62906bcb845bf46
ned3144 <nadav.misgav@gmail.com>
2022-08-05 22:39:15 +0300

    add 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,6 @@
+// pages/404.js
+export default function Custom404() {
+    return (<div className="flex h-screen justify-center items-center">
+        <h1 className="text-4xl">404 | העמוד המבוקש לא נמצא ☹</h1>
+    </div>)
+}
