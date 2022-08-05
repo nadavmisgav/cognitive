621a03c1fe4185cc6a2316d4895a28186a7427dd
ned3144 <nadav.misgav@gmail.com>
2022-08-05 22:55:09 +0300

    basic post page

--- src/pages/posts/[postId].tsx
+++ src/pages/posts/[postId].tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import { trpc } from '../../utils/trpc'
+import Custom404 from '../404'
 
 function SinglePostPage() {
     const router = useRouter()
@@ -9,13 +10,21 @@ function SinglePostPage() {
     const { data, isLoading } = trpc.useQuery(['posts.single-post', { postId }])
 
     if (isLoading) {
-        return <></>
+        return <h1>טוען</h1>
+    }
+
+    if (!data) {
+        return <Custom404 />
     }
 
     return (
         <div>
-            <h1>{data?.title}</h1>
-            <p>{data?.body}</p>
+            <img className="w-full opacity-40 max-h-[30vh]" src={data.image} />
+            <h1 className='text-center mt-12 text-5xl font-bold tracking-wider sm:text-6xl'>{data.title}</h1>
+            <h6 className='text-center text-sm text-gray-300 sm:text-base'>{data.category} / {data.createdAt.toLocaleDateString("he")}</h6>
+            <div className='mt-8 py-8 px-12 bg-gray-600 rounded-t-md sm:w-4/5 mx-auto max-w-6xl'>
+                {data.body}
+            </div>
         </div>
     )
 }
