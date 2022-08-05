d94cb577f004072b0a23812753123fa427f2eeff
ned3144 <nadav.misgav@gmail.com>
2022-08-05 22:58:29 +0300

    ui changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,6 @@ const Navbar: React.FC<{}> = () => {
         <nav className="text-gray-300 m-6 xl:text-xl xl:ml-[12%] ">
             <ul className="p-0 flex list-none justify-end">
                 <li className="m-3 hover:text-gray-100 hover:underline hover:cursor-pointer"><Link href="/">ראשי</Link></li>
-                <li className="m-3 hover:text-gray-100 hover:underline hover:cursor-pointer">אודות</li>
                 <li className="m-3 hover:text-gray-100 hover:underline hover:cursor-pointer">רשימת פוסטים</li>
                 <li className="m-3 hover:text-gray-100 hover:underline hover:cursor-pointer">קטגוריות</li>
             </ul>
--- src/pages/posts/[postId].tsx
+++ src/pages/posts/[postId].tsx
@@ -19,10 +19,10 @@ function SinglePostPage() {
 
     return (
         <div>
-            <img className="w-full opacity-40 max-h-[30vh]" src={data.image} />
+            <img className="w-full opacity-40 max-h-[30vh] xl:max-h-[40vh]" src={data.image} />
             <h1 className='text-center mt-12 text-5xl font-bold tracking-wider sm:text-6xl'>{data.title}</h1>
             <h6 className='text-center text-sm text-gray-300 sm:text-base'>{data.category} / {data.createdAt.toLocaleDateString("he")}</h6>
-            <div className='mt-8 py-8 px-12 bg-gray-600 rounded-t-md sm:w-4/5 mx-auto max-w-6xl'>
+            <div className='mt-8 py-8 px-12 bg-gray-600 rounded-t-md sm:w-4/5 mx-auto max-w-6xl text-gray-200'>
                 {data.body}
             </div>
         </div>
