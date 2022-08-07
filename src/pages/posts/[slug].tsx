733547b149e935b9d4f9083f47f4bb79a8277a96
ned3144 <nadav.misgav@gmail.com>
2022-08-07 23:37:17 +0300

    add margin

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -42,7 +42,7 @@ const SinglePostPage: React.FC<{ source: MDXRemoteSerializeResult, frontMatter: 
             <div>
                 <img className="w-full opacity-40 max-h-[30vh] xl:max-h-[40vh]" src={frontMatter.image} />
                 <h1 className='text-center mt-12 text-5xl font-bold tracking-wider sm:text-6xl'>{frontMatter.title}</h1>
-                <h6 className='text-center text-sm text-gray-300 sm:text-base'>{frontMatter.category} / {frontMatter.createdAt}</h6>
+                <h6 className='m-2 text-center text-sm text-gray-300 sm:text-base'>{frontMatter.category} / {frontMatter.createdAt}</h6>
                 <div className='mt-8 py-8 px-4 bg-gray-600 rounded-t-md sm:w-4/5 mx-auto max-w-6xl text-gray-200'>
                     <MDXRemote {...source} components={components} />
                 </div>
