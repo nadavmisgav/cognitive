26465e42c688c8d5a4652ebb291e63f3d4f83334
ned3144 <nadav.misgav@gmail.com>
2022-08-07 23:31:55 +0300

    add code color

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -23,7 +23,7 @@ const components = {
     img: (props: any) => <img className="mx-auto" {...props} />,
     p: (props: any) => <p className="mb-4" {...props} />,
     blockquote: (props: any) => <blockquote className="my-4 italic bg-gray-800 p-4 rounded-xl text-gray-300" {...props} />,
-    code: (props: any) => <code style={{ direction: "ltr", borderRadius: "0.75rem" }} className="italic py-[0.1rem] px-1 inline-block" {...props} />,
+    code: (props: any) => <code style={{ backgroundColor: "rgb(31, 41, 55)", direction: "ltr", borderRadius: "0.75rem" }} className="italic py-[0.1rem] px-2 inline-block" {...props} />,
     li: (props: any) => <li className="list-disc mr-12" {...props} />,
     a: (props: any) => <a className='hover:underline italic' {...props} />,
 }
