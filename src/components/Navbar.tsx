c3b21b0cce02edcc55779cccb5fa7d9156b17e0a
ned3144 <nadav.misgav@gmail.com>
2022-10-20 00:00:57 +0300

    add github icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
 import Link from 'next/link';
 import React from "react";
 
+import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
+import { useRouter } from 'next/router';
+
 const Navbar: React.FC<{}> = () => {
+    const { asPath } = useRouter()
+    const isHomePage = asPath === "/"
+    
     return (
-        <nav className="text-gray-300 m-6 xl:text-xl xl:ml-[12%] ">
+        <nav className="text-gray-300 my-6 mx-auto text-2xl xl:text-3xl xl:w-[70%] ">
             <ul className="p-0 flex list-none justify-end">
-                <li className="m-3 hover:text-gray-100 hover:underline hover:cursor-pointer"><Link href="/">ראשי</Link></li>
-                <li className="m-3 hover:text-gray-100 hover:underline hover:cursor-pointer">רשימת פוסטים</li>
-                <li className="m-3 hover:text-gray-100 hover:underline hover:cursor-pointer">קטגוריות</li>
+                {!isHomePage && <li className="m-3 hover:text-gray-100 hover:underline hover:cursor-pointer ml-auto"><Link href="/">קוגניטיבי</Link></li>}
+                <li className="m-3 hover:text-gray-100 hover:underline hover:cursor-pointer text-2xl xl:text-3xl">
+                    <a target="_blank" href="https://github.com/nadavmisgav"><FaGithub /></a>
+                </li>
             </ul>
         </nav>
     )
